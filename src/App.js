7776974ef9f81128ca003cefb77e455fc5f518ae
Mugisha Kakou <joycechristiane23@gmail.com>
2020-01-06 23:03:18 +0100

    little changes

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <h1>UnicornLand Cats</h1>
+        <h1 className='header'>UnicornLand Cats</h1>
         <SearchBox placeholder="Search a UnicornLand cat"  handleChange={this.handleChange}/>
         <CardList unicorns={filteredUnicorns} />
       </div>
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,9 +5,9 @@ import React from 'react';
 const Card = ({unicorn,...props}) => {
     return  (
         <div className='card-container'>
-        <img alt='unicorn' src={`https://robohash.org/${unicorn.id}?set=set4&bgset=&size=180x180`} />
+        <img alt='unicorn' src={`https://robohash.org/${unicorn.id}?set=set4&bgset=&size=150x150`} />
         <h2>{unicorn.name}</h2>
-        <p>{unicorn.email}</p>
+        
         </div>
         );
 };
