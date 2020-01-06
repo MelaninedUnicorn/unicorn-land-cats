3d5d350afbf7f850d9e31cf4892ac8b3ff58fa50
Mugisha Kakou <joycechristiane23@gmail.com>
2020-01-06 22:25:11 +0100

    Updates

---
+++ precache-manifest.f0a922c803dd419bdbef25426cfb4218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4732544fc2c6ab6062db2b8469b1698",
+    "url": "/unicorn-land-cats/index.html"
+  },
+  {
+    "revision": "b5d5430321dbd54fa8eb",
+    "url": "/unicorn-land-cats/static/css/main.fb821f78.chunk.css"
+  },
+  {
+    "revision": "5c8b52e5bd6edefab1cc",
+    "url": "/unicorn-land-cats/static/js/2.84253bb0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/unicorn-land-cats/static/js/2.84253bb0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5d5430321dbd54fa8eb",
+    "url": "/unicorn-land-cats/static/js/main.42e4ee22.chunk.js"
+  },
+  {
+    "revision": "7a7e7543886e1ec49eeb",
+    "url": "/unicorn-land-cats/static/js/runtime-main.c8bbde17.js"
+  }
+]);
